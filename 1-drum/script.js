01dbf9705f6347d6f10f3474038b35cb7d0c3341
Patagonikus <corvalanemiliano@hotmail.com>
2022-01-20 23:47:23 -0300

    Ejercicio 1

---
+++ 1-drum/script.js
@@ -0,0 +1,17 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(audio){
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add("playing");
+        setTimeout(function(){
+            key.classList.remove("playing");
+
+        },1.07);
+    }else{
+        console.log("no hay tecla asociada");
+    }
+}
+
+window.addEventListener('keydown', playSound);
